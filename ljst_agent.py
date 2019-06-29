9a2d6914d9fc639f1125200b0d7c7f45bf2bdaf7
liangxianfeng <liangxianfeng@cqu.edu.cn>
2019-06-29 23:51:21 +0800

    Update ljst_agent.py

--- ljst_agent.py
+++ ljst_agent.py
@@ -3,19 +3,22 @@ import paddle.fluid as fluid
 import parl.layers as layers
 from parl.framework.agent_base import Agent
 
-STATE_SIZE = (260,1)
+STATE_SIZE = (176,1)
 CONTEXT_LEN = 4
 
 
 class LJST_Agent(Agent):
-    def __init__(self, algorithm, action_dim):
+    def __init__(self, algorithm, action_dim, station_num, max_scheduling_num):
         super(LJST_Agent, self).__init__(algorithm)
 
         self.exploration = 1.1
-        self.action_dim = action_dim
+        
         self.global_step = 0
         self.update_target_steps = 10000 // 4
-
+        self.station_num = station_num
+        self.max_scheduling_num = max_scheduling_num
+        self.action_dim = action_dim
+        
     def build_program(self):
         self.pred_program = fluid.Program()
         self.learn_program = fluid.Program()
@@ -32,50 +35,56 @@ class LJST_Agent(Agent):
                 name='obs',
                 shape=[CONTEXT_LEN, STATE_SIZE[0], STATE_SIZE[1]],
                 dtype='float32')
-            action = layers.data(name='act', shape=[1], dtype='int32')
+            action = layers.data(name='act', shape=[-1,17])
+#             action1 = layers.data(name='act1', shape=[8])
             reward = layers.data(name='reward', shape=[], dtype='float32')
             next_obs = layers.data(
                 name='next_obs',
                 shape=[CONTEXT_LEN, STATE_SIZE[0], STATE_SIZE[1]],
                 dtype='float32')
             terminal = layers.data(name='terminal', shape=[], dtype='bool')
-            self.cost = self.alg.define_learn(obs, action, reward, next_obs,
-                                              terminal)
+            self.cost = self.alg.define_learn(obs, action, reward,next_obs,terminal)
 
     def sample(self, obs):
         sample = np.random.random()
         if sample < self.exploration:
-            act = np.random.randint(0,9,size=(self.action_dim,))
+            ed = st = np.zeros(self.station_num)
+            st[np.random.randint(0,self.station_num)] = 1  
+            ed[np.random.randint(0,self.station_num)] = 1
+            num = np.random.randint(0,self.max_scheduling_num)
+            act = np.concatenate([st,ed,[num]])
         else:
             if np.random.random() < 0.01:
-                act = np.random.randint(0,9,size=(self.action_dim,))
+                st = np.zeros(self.station_num)
+                ed = np.zeros(self.station_num)
+                st[np.random.randint(0,self.station_num)] = 1  
+                ed[np.random.randint(0,self.station_num)] = 1
+                num = np.random.randint(0,self.max_scheduling_num) *10
+                act = np.concatenate([st,ed,[num]])
             else:
-#                 obs = np.expand_dims(obs, axis=0)
+                st = np.zeros(self.station_num)
+                ed = np.zeros(self.station_num)
+                obs = np.expand_dims(obs, axis=0)
                 pred_Q = self.fluid_executor.run(
                     self.pred_program,
                     feed={'obs': obs.astype('float32')},
                     fetch_list=[self.value])[0]
                 pred_Q = np.squeeze(pred_Q, axis=0)
-                act = np.argmax(pred_Q)
-        self.exploration = max(0.1, self.exploration - 1e-6)
+                print('predQ',pred_Q.shape)
+    #                 act = np.argmax(pred_Q)
+                st[np.argmax(pred_Q[0:8])] = 1
+                ed[np.argmax(pred_Q[8:16])] = 1
+                num = int(pred_Q[16]) * 10
+                print('agent_num',num)
+                act = np.concatenate([st,ed,[num]])
+                print('agent_act',act)
+        self.exploration = max(0.1, self.exploration - 1e-5)
         return act
 
-    def predict(self, obs):
-        
-#         obs = np.expand_dims(obs, axis=0)
-        print('pred_obs',obs.shape)
-        pred_Q = self.fluid_executor.run(
-            self.pred_program,
-            feed={'obs': obs.astype('float32')},
-            fetch_list=[self.value])[0]
-        pred_Q = np.squeeze(pred_Q, axis=0)
-        act = np.argmax(pred_Q)
-        print('pred_act',act.shape)
-        return act
 
     def learn(self, obs, act, reward, next_obs, terminal):
-        if self.global_step % self.update_target_steps == 0:
-            self.alg.sync_target(self.gpu_id)
+#         if self.global_step % self.update_target_steps == 0:
+#             self.alg.sync_target(self.gpu_id)
         self.global_step += 1
 
 #         act = np.expand_dims(act, -1)
