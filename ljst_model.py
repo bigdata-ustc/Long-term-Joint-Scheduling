68b41c8f8dd54b275cf5cca52778953ded430581
liangxianfeng <liangxianfeng@cqu.edu.cn>
2019-07-04 20:32:17 +0800

    Update ljst_model.py

--- ljst_model.py
+++ ljst_model.py
@@ -4,41 +4,12 @@ import parl.layers as layers
 from parl.framework.model_base import Model
 
 
-# class LJST_Model(Model):
-#     def __init__(self, act_dim):
-#         self.act_dim = act_dim
-
-#         self.conv1 = layers.conv2d(
-#             num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
-#         self.conv2 = layers.conv2d(
-#             num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
-#         self.conv3 = layers.conv2d(
-#             num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
-#         self.conv4 = layers.conv2d(
-#             num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
-#         self.fc1 = layers.fc(size=act_dim)
-
-#     def value(self, obs):
-# #         obs = obs / 255.0
-#         out = self.conv1(obs)
-#         out = layers.pool2d(
-#             input=out, pool_size=2, pool_stride=2, pool_type='max')
-#         out = self.conv2(out)
-#         out = layers.pool2d(
-#             input=out, pool_size=2, pool_stride=2, pool_type='max')
-#         out = self.conv3(out)
-#         out = layers.pool2d(
-#             input=out, pool_size=2, pool_stride=2, pool_type='max')
-#         out = self.conv4(out)
-#         out = layers.flatten(out, axis=1)
-#         out = self.fc1(out)
-#         return out
 class LJST_Model(Model):
     def __init__(self, act_dim):
         self.act_dim = act_dim
-
+        
         self.conv1 = layers.conv2d(
-            num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
+            num_filters=32, filter_size=2, stride=1, act='relu')
         self.conv2 = layers.conv2d(
             num_filters=8, filter_size=4, stride=1, padding=2, act='relu')
         self.conv3 = layers.conv2d(
@@ -51,27 +22,23 @@ class LJST_Model(Model):
 
     def value(self, obs):
 #         obs = obs / 255.0
+        print('value',obs.shape)
         out = self.conv1(obs)
         out = layers.pool2d(
-            input=out, pool_size=2, pool_stride=2, pool_type='max')
-        out = self.conv2(out)
-        out = layers.pool2d(
-            input=out, pool_size=2, pool_stride=2, pool_type='max')
-        out = self.conv3(out)
-        out = layers.pool2d(
-            input=out, pool_size=2, pool_stride=2, pool_type='max')
-        out = self.conv4(out)
+            input=out, pool_size=4, pool_stride=2, pool_type='max')
+#         out = self.conv2(out)
+#         out = layers.pool2d(
+#             input=out, pool_size=2, pool_stride=2, pool_type='max')
+#         out = self.conv3(out)
+#         out = layers.pool2d(
+#             input=out, pool_size=2, pool_stride=2, pool_type='max')
+#         out = self.conv4(out)
         out = layers.flatten(out, axis=1)
         start = self.fc1(out)
         start = paddle.fluid.layers.softmax(start)
         end = self.fc2(out)
         end = paddle.fluid.layers.softmax(end)
         num = self.fc3(out)
-#         print('start',start.shape)
-#         print('end',end.shape)
-#         print('num',num.shape)
         vec = layers.concat([start,end,num],axis=1)
-#         print('model_shape: ',vec.shape)
         return vec
-#         return start
     
